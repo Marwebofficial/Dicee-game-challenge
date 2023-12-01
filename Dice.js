7b36060f62db040bff556c2860e44c3e78112aa9
Marwebofficial <141563399+Marwebofficial@users.noreply.github.com>
2023-12-01 16:25:06 +0100

    Update Dice.js

--- Dice.js
+++ Dice.js
@@ -1,10 +1,10 @@
 var randomNo = Math.floor(Math.random()*6) + 1;
-var randomimage1 = "images/dice" + randomNo + ".png";
+var randomimage1 = "dice" + randomNo + ".png";
 var randomimgsource =document.querySelector(".img1").setAttribute("src",
 randomimage1);
 
 var randomNo2 = Math.floor(Math.random()*6) + 1;
-var randomimage2 = "images/dice" + randomNo2 + ".png";
+var randomimage2 = "dice" + randomNo2 + ".png";
 var randomimgsource2 =document.querySelector(".img2").setAttribute("src",
 randomimage2);
 if (randomNo > randomNo2) {
